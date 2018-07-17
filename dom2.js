305ba3de2d76bb6ab748b1f5d77a6d2059914831
Ugur Kirmaci <kirmaciugur@gmail.com>
2018-07-17 16:17:18 +0300

    Gonderildi

---
+++ dom2.js
@@ -0,0 +1,14 @@
+var button = document.querySelector("button");
+/*var isRed = false;
+
+button.addEventListener("click",function(){
+	if (isRed) {
+		document.body.style.background= "white";
+	}else {
+		document.body.style.background ="red";
+	}
+	isRed = !isRed
+});*/
+button.addEventListener("click", function () {
+	document.body.classList.toggle("red");
+});
